16c03d13ba4ee304c857fc2b193277c4f7c100aa
German Bertea <german.bertea@altran.com>
2021-08-07 19:46:02 +0200

    order products

--- controllers/order.js
+++ controllers/order.js
@@ -38,19 +38,24 @@ exports.getAllProductsFromOrder = async (req, res, next) => {
   let orderId = req.params.orderId;
   try {
     const orderItems = await order_items.findAll({
+      attributes: ["products.id"],
       where: { orderId: orderId },
       include: "products",
     });
+    // var products = [];
+    // orderItems.products.forEach((element) => {
+    //   products.push(...element);
+    // });
 
     if (!orderItems) {
       const error = new Error("No Order was found with this Id");
       error.statusCode = 200;
       throw error;
     }
-
+    console.log(orderItems);
     res.status(200).json({
-      message: "Fetched posts succesfully",
-      orderItems,
+      message: "Fetched Products succesfully",
+      products: orderItems,
     });
   } catch (err) {
     if (!err.statusCode) {
